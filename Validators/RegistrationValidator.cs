ec2d869281a44286ca1d3b52231c467294e00e20
assasinos <kubasem198@gmail.com>
2023-06-09 20:50:40 +0200

    Add Registration Validator

---
+++ Validators/RegistrationValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WebApplication3.Models;
+
+namespace WebApplication3.Validators;
+
+public class RegistrationValidator : AbstractValidator<RegisterModel>
+{
+    public RegistrationValidator()
+    {
+        RuleFor(x => x.password).NotEmpty().WithMessage("Password is required");
+        RuleFor(x => x.firstname).NotEmpty().WithMessage("First Name is required");
+        RuleFor(x => x.surname).NotEmpty().WithMessage("Surname is required");
+        RuleFor(x => x.username).NotEmpty().WithMessage("Username is required");
+        RuleFor(x => x.email).NotEmpty().WithMessage("E-Mail is required").EmailAddress().WithMessage("E-Mail is not valid");
+
+    }    
+}
