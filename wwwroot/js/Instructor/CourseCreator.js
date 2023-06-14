af728c7dbe22e29e331b6de8e68a07542047c7b9
assasinos <kubasem198@gmail.com>
2023-06-14 17:51:28 +0200

    Add Front end Javascript to handle Course creator action

---
+++ wwwroot/js/Instructor/CourseCreator.js
@@ -0,0 +1,25 @@
+﻿
+
+
+
+onsubmit = (e) => {
+    e.preventDefault();
+    CreateCourse();
+};
+function CreateCourse(){
+    const form = $("#CourseFrom").serialize();
+    $.ajax({
+        url: "/api/instructor/CourseEdit/CreateCourse",
+        type: "PUT",
+        data: form,
+        success: function (data) {
+            prepareToast("CheckCircle.svg", "Success", "Course created successfully You will be moved shortly");
+            setTimeout(() => {
+                window.location.href = "/Instructor/Course/" + data;
+            }, 2000);
+        },
+        error: function (data) {
+            prepareToast("xmark.svg", "Error", data.responseJSON.errorMessage);
+        }
+    });
+}
