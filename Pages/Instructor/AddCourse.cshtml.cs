1a34067904b21e3358abe54d6a89b8f8eb029209
assasinos <kubasem198@gmail.com>
2023-06-14 17:52:30 +0200

    Add Front End for creating course

---
+++ Pages/Instructor/AddCourse.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using ELearnApp.Models;
+using ELearnApp.Validators;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ELearnApp.Pages.Instructor;
+
+[IsInRole(UserRole.Instructor)]
+public class AddCourse : PageModel
+{
+    public async Task<IActionResult> OnGet()
+    {
+        return Page();
+    }
+}
